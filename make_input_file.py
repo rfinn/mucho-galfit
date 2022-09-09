99bf27b655fb13d13e99cf56eedcca010a916eee
Kim Conger <39608848+Kyssuber@users.noreply.github.com>
2022-09-09 16:07:36 -0400

    Add files via upload

---
+++ make_input_file.py
@@ -0,0 +1,17 @@
+
+import os
+import numpy as np
+
+homedir=os.getenv("HOME")
+    
+def write_file():    
+    vf = Table.read(homedir+'/v2_snrcoadd.fits')
+    vf = vf[vf['sgacut_flag']]
+    file_obj = open('galaxy_file.txt','w')
+    for i in range(0,len(vf)):
+        ra = vf['RA_1'][i]
+        ra_int = int(np.floor(ra))
+        objname = vf['objname'][i]
+        file_obj.write(str(ra_int)+'/'+objname)
+        file_obj.write('\n')
+    file_obj.close()
