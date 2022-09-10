b37ab1f278d23c39e32626dee253122da21030c7
rfinn <rfinn>
2022-09-10 17:16:41 -0400

    making more progress

--- parallel/run1galfit.py
+++ parallel/run1galfit.py
@@ -163,9 +163,10 @@ def write_galfit_input(galdir, output_dir, bandpass, firstpass=True):
         sky = 0
     else:
         # read in output from first pass run of galfit
-        fit_parameters = parse_galfit_output(output_image.replace('out2','out1'))
+        t = parse_galfit_output(output_image.replace('out2','out1'))
+        print(fit_parameters)
         # header_keywords=['1_XC','1_YC','1_MAG','1_RE','1_N','1_AR','1_PA','2_SKY','CHI2NU']
-        xc, yc, mag, rad, nsersic, BA, PA, sky = fit_parameters[np.arange(0,15,2)]
+        xc, yc, mag, rad, nsersic, BA, PA, sky = t[0],t[2],t[4],t[6],t[8],t[10],t[12],t[14]
         fitBA = 1
         fitPA = 1
         fitn = 1
