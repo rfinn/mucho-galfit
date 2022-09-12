c5dfb10fc99c7c0e089f36cf6655ede40cb92116
Kim Conger <39608848+Kyssuber@users.noreply.github.com>
2022-09-12 10:46:19 -0500

    adding brief documentation

--- make_input_file.py
+++ make_input_file.py
@@ -1,11 +1,17 @@
 
+'''
+Goal: generate .txt input file for running galfit in parallel on all sample galaxies.
+-accepts vf subsample. already defined as a variable below, so only need to call write_file() to run.
+'''
+
 import os
 import numpy as np
 from astropy.table import Table
 
 homedir=os.getenv("HOME")
     
-def write_file():    
+def write_file():
+    #table contains all v2 galaxies; 'sgacut_flag' is the flag column which trims vf such that only galaxies are those in sga_coadd subsample
     vf = Table.read(homedir+'/v2_snrcoadd.fits')
     vf = vf[vf['sgacut_flag']]
     file_obj = open('galaxy_file.txt','w')
