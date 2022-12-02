bcf36952d6242e33c857574ce86e5799828bd19c
rfinn <rfinn@siena.edu>
2022-12-02 18:21:11 -0500

    think I've cracked the typing-extensions conundrum...

--- parallel/mksbatchfile.py
+++ parallel/mksbatchfile.py
@@ -158,7 +158,7 @@ input_file = "Dirs.txt"
 # set sample size to 10 galaxies for testing
 
 if args.testsample:
-    nfiles=5
+    nfiles=10
     print(f"\nSetting number of jobs to {nfiles} for testing.\n")
     
 write_output(script_id, input_file, narray=nfiles, data_dir=data_dir, \
