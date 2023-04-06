11fdff91549fb52c4e99c4899dfdc3337aee0239
rfinn <rfinn>
2023-04-06 13:27:11 -0400

    code to find group galaxies

--- parallel/run1galfitgroup.py
+++ parallel/run1galfitgroup.py
@@ -337,7 +337,7 @@ def write_galfit_input(galdir, output_dir, objname, ra, dec, bandpass, firstpass
             elif line.startswith('I)'):
                 outfile.write('I) '+str(int(round(convolution_size)))+' '+str(int(round(convolution_size)))+'             # Size of convolution box (x y)\n')
                 
-            elif line.startswith('3)'):# check if mag is too faint, then don't fit for new parameters
+            elif line.startswith(' 3)'):# check if mag is too faint, then don't fit for new parameters
                 t = line.split()
                 # check the magnitude returned from no covolution
                 print("checking magnitude {float(t[1])} compared to {minmag}")
@@ -347,7 +347,7 @@ def write_galfit_input(galdir, output_dir, objname, ra, dec, bandpass, firstpass
                     outfile.write(line.replace(' 1 ',' 0 '))
                 else:
                     holdfixed=False
-            elif holdfixed and (line.startswith('(4') or line.startswith('(5') or line.startswith('(9') or line.startswith('(10')):
+            elif holdfixed and (line.startswith(' (4') or line.startswith(' (5') or line.startswith(' (9') or line.startswith('(10')):
                 outfile.write(line.replace(' 1 ',' 0 '))
             else:
                 outfile.write(line)
