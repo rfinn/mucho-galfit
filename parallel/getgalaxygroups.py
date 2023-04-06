bc57b3daa59dc3f15a9b4d0adafcefc831bd6e8d
rfinn <rfinn>
2023-04-06 10:36:08 -0400

    code to find group galaxies

--- parallel/getgalaxygroups.py
+++ parallel/getgalaxygroups.py
@@ -36,7 +36,7 @@ groupflag = np.zeros(len(dirlist),'bool')
 primaryflag = np.zeros(len(dirlist),'bool')
 
 groupdirs_list = []
-
+primaryvfid=[]
 for i,dir in enumerate(dirlist):
     #print(i,os.getcwd(),dir)
     # search for galaxies with no fits files
@@ -61,14 +61,15 @@ for i,dir in enumerate(dirlist):
         # check to see if this is the primary galaxy
 
         group_dir = f'/mnt/astrophysics/virgofilaments-data/{int(ra)}/{objname}_GROUP'
-        print(group_dir)
+        #print(group_dir)
         if os.path.exists(group_dir):
             primaryflag[i] = True
-            print('group and found primary')
+            #print('group and found primary')
             groupdirs_list.append(group_dir)
+            primaryvfids.append(dir)
             os.chdir(topdir)
         else:
-            print('group and but not primary')            
+            #print('group and but not primary')            
             os.chdir(topdir)
             continue
 
@@ -77,6 +78,12 @@ for i,dir in enumerate(dirlist):
 outfile = open('groupDirs.txt','w')
 for g in groupdirs_list:
     outfile.write(g+'\n')
+outfile.close()
+
+# write out list of groupdirs
+outfile = open('groupPrimaryVFID.txt','w')
+for p in primaryvfids:
+    outfile.write(p+'\n')
 outfile.close()
 
 # save information about group
