d692475f728a97d37ae153bae6be85561d78fb3d
rfinn <rfinn>
2024-06-04 22:18:06 -0400

    adding 3rd round to hold sersic index fixed for solutions with n > 6

--- parallel/run1galfitgroup.py
+++ parallel/run1galfitgroup.py
@@ -117,7 +117,7 @@ def get_galfit_nsersic(galfit_output_image,ngal=1):
         # parse output : '*16.1052* +/- *1545.5958*'
         n = float(t.replace('*','').split('+/-')[0])
         if n > 6:
-            nsersic_flag[i] = True
+            high_nsersic_flag[i] = True
         nsersic[i] = t
 
     return nsersic, high_nsersic_flag
