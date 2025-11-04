98be103f936c0cfd83e08af66cfc738519789dce
Kyssuber <k22cong@siena.edu>
2025-11-04 16:16:38 -0600

    tweaking params.txt

--- parallel/run1directory.py
+++ parallel/run1directory.py
@@ -10,9 +10,7 @@ import sys
 from astropy.table import Table
 
 # --- Import utility functions ---
-sys.path.insert(0, '../utils')
-sys.path.insert(0, '../')
-sys.path.insert(0, 'utils')
+sys.path.insert(0, '~/github/mucho-galfit/utils')
 from setup_galfit_ws import setup_one_galaxy
 from getprimarydirs import write_primarydirs, find_primaries
 
