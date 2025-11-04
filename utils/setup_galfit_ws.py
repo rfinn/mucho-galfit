88773247bf3b22b62bdfe95317325128f7c67a66
Kyssuber <k22cong@siena.edu>
2025-11-04 16:49:34 -0600

    tweaking params.txt

--- utils/setup_galfit_ws.py
+++ utils/setup_galfit_ws.py
@@ -168,9 +168,9 @@ def get_images(objid,ra,dec,output_loc,data_root_dir,hemisphere_bound=32.):
     ra_slice = f'{int(ra):03d}'
         
     if dec>hemisphere_bound:
-        data_dir = os.path.abspath(f'{data_root_dir}dr11-north/{ra_slice}/')+'/'
+        data_dir = os.path.abspath(f'{data_root_dir}dr11-north/{ra_slice}/')
     if dec<hemisphere_bound:
-        data_dir = os.path.abspath(f'{data_root_dir}dr11-south/{ra_slice}/')+'/'
+        data_dir = os.path.abspath(f'{data_root_dir}dr11-south/{ra_slice}/')
         
     if not os.path.exists(data_dir):
         print(f"could not find data_dir {data_dir} - exiting")
@@ -298,6 +298,7 @@ if __name__ == '__main__':
         os.chdir(outdir)
     else:
         os.system(f'mkdir {outdir}')
+        os.chdir(outdir)
     
     #I need to keep some living array of failure modes...
     #and also print the problems in the terminal so I know WHY they failed
